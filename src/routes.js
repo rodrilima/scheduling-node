8f2407fdd6496a774dd948c865e3fd45217a6769
Rodrigo Lima <rodelima20@gmail.com>
2020-03-09 04:55:28 -0300

    chore: create view signin

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,10 @@ const { Router } = require('express')
 const routes = new Router();
 
 routes.get('/', (req,res) => {
-    return res.render('auth/singup', { pagetitle: "Singup" })
+    return res.render('auth/signin', { pagetitle: "Sing In" })
+})
+routes.get('/signup', (req,res) => {
+    return res.render('auth/signup', { pagetitle: "Sign Up" })
 })
 
 module.exports = routes
